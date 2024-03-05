7361295e57942880f821cd4948068fe41e91238a
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2024-03-05 15:58:15 +0100

    Esercizio 4 ok

---
+++ src/Esercizio3/Esercizio3.java
@@ -0,0 +1,25 @@
+package Esercizio3;
+
+import java.sql.Array;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Esercizio3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        boolean openLoop = true;
+        while (openLoop){
+        System.out.println("Vuoi chiudere il Loop? Se si inserisci :q");
+        String risposta = scanner.nextLine();
+
+
+        if (Objects.equals(risposta, ":q")) {
+            openLoop = false;
+            scanner.close();
+            System.out.println("Loop in chiusura");
+        }
+    }
+    }
+
+}
---
+++ src/Esercizio4/Esercizio4.java
@@ -0,0 +1,20 @@
+package Esercizio4;
+
+import java.util.Scanner;
+
+public class Esercizio4 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci un numero");
+        int risposta = scanner.nextInt();
+
+       if (risposta > 0){
+           for (int i = risposta; i >= 1; i--) {
+               System.out.println(i);
+                scanner.close();
+       }
+
+        }
+    }
+}
